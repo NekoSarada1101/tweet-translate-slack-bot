328c4eb30648d497c51dd6d77275c122286ffcc1
NekoSarada1101 <ryoyukikazu11@gmail.com>
2021-01-25 18:46:04 +0900

    feat(settings.py): アカウントごとに切り替え

---
+++ settings.py
@@ -0,0 +1,15 @@
+SLACK_WEBHOOK_URL = ""
+ICON_NAME = ""
+
+
+# アカウントごとにSlackWebhookURLとアイコンを設定する
+def set_settings(username: str):
+    if username == "username1":
+        "SLACK_WEBHOOK_URL1"
+        "ICON_NAME1"
+    elif username == "username2":
+        "SLACK_WEBHOOK_URL2"
+        "ICON_NAME2"
+    elif username == "username3":
+        "SLACK_WEBHOOK_URL3"
+        "ICON_NAME3"
