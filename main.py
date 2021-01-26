914437142fee6cc6ee0bb21715bc4eeea1415d71
NekoSarada1101 <ryoyukikazu11@gmail.com>
2021-01-26 18:04:25 +0900

    feat(main.py): レスポンスをログ出力

--- main.py
+++ main.py
@@ -24,4 +24,5 @@ def do_post(request):
         "unfurl_links": "true",
     }
     payload = json.dumps(data).encode("utf-8")  # type: json
-    requests.post(SLACK_WEBHOOK_URL, payload)
+    response = requests.post(SLACK_WEBHOOK_URL, payload)
+    print(response)
