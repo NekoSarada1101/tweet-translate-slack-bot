f3a382706d1ab90bfbcb524bc4264e6b4c7d20f8
NekoSarada1101 <ryoyukikazu11@gmail.com>
2021-01-28 00:57:38 +0900

    fix(settings.py): 変数をグローバル指定

--- settings.py
+++ settings.py
@@ -4,14 +4,18 @@ ICON_NAME = ""
 
 # アカウントごとにSlackWebhookURLとアイコンを設定する
 def set_settings(username: str):
+    global SLACK_WEBHOOK_URL
+    global ICON_NAME
+    
     if username == "username1":
-        SLACK_WEBHOOK_URL = "SLACK_WEBHOOK_URL1"
-        ICON_NAME = "ICON_NAME1"
+        "SLACK_WEBHOOK_URL1"
+        "ICON_NAME1"
     elif username == "username2":
-        SLACK_WEBHOOK_URL = "SLACK_WEBHOOK_URL2"
-        ICON_NAME = "ICON_NAME2"
+        "SLACK_WEBHOOK_URL2"
+        "ICON_NAME2"
     elif username == "username3":
-        SLACK_WEBHOOK_URL = "SLACK_WEBHOOK_URL3"
-        ICON_NAME = "ICON_NAME3"
+        "SLACK_WEBHOOK_URL3"
+        "ICON_NAME3"
+    
     print(SLACK_WEBHOOK_URL)
     print(ICON_NAME)
